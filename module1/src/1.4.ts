47dff25f28c917aa2bb82d11952a73313c064ab0
shaon22 <shaonroy11@gmail.com>
2025-06-10 22:28:15 +0600

    explored basic data types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,21 @@
+// basic data types
+
+//string
+
+let firstName : string="shaon"
+
+//number
+
+let ID : number= 611
+
+//boolean
+
+let isAdmin : boolean= true
+
+//undefined
+
+let x: undefined= undefined
+
+//null
+
+let a: null= null
