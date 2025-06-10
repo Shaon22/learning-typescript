70a483ea52d07faed25774a80094f2b996afc564
shaon22 <shaonroy11@gmail.com>
2025-06-11 00:18:54 +0600

    explored spread operator and rest operator

---
+++ module1/src/1.7.ts
@@ -0,0 +1,33 @@
+{
+//spread operator
+// Distructuring
+//rest operator
+
+//spread operation
+
+//with array:
+
+const bros1:string[]=['saif','siam','taim']
+const bros2:string[]=['mamun','ashim','nirob']
+bros1.push(...bros2)
+//with object
+
+const mentor1={
+    ts:"mezba",
+    redux:"mir"
+}
+const mentor2={
+    next:"tonmoy",
+    cloud:"mizan"
+}
+const mentorList={
+    ...mentor1,
+    ...mentor2
+}
+
+
+
+
+}
+
+
