071ea5417c23df994e5f35a0adb87a65670a1822
shaon22 <shaonroy11@gmail.com>
2025-06-11 16:19:08 +0600

    explored type alias with object

---
+++ module1/src/1.9.ts
@@ -0,0 +1,24 @@
+// type alias
+type student={
+    name:string,
+    age:number,
+    contactNo?:number,
+    gender:string
+}
+const student1:student={
+    name:"shaon",
+    age:25,
+    contactNo:1794349467,
+    gender:'male'
+}
+const student2:student={
+    name:"polock",
+    age:25,
+    contactNo:1794349467,
+    gender:'male'
+}
+const student3:student={
+    name:"roy",
+    age:25,
+    gender:'male'
+}
